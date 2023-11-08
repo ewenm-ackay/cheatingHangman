c89d476180955e2ef4a2921d745a90c2cbdcef60
ewenm-ackay <150217867+ewenm-ackay@users.noreply.github.com>
2023-11-08 00:37:27 -0500

    Update cheatingHangmanLogic.py

--- cheatingHangmanLogic.py
+++ cheatingHangmanLogic.py
@@ -1,1 +1,4 @@
 # basic logic for cheating hangman written in python
+# we dont want to write the whole game in here, this is just experimental
+# kind of like a brainstorming page. any features we want to add should start
+# in this file
