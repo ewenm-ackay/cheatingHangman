3e34eb96020743d34d26ac7436e97d269da69f31
ewen <ewenmackayy@gmail.com>
2023-12-07 17:44:36 -0500

    Made some changes

--- cheatingHangmanLogic.py
+++ cheatingHangmanLogic.py
@@ -5,6 +5,7 @@
 
 from time import sleep
 
+cheatword = "Dr. Rosen"
 # initialize game with needed parameters
 
 def slowPrint(string, delay = .1, end="\n"):
@@ -13,6 +14,7 @@ def slowPrint(string, delay = .1, end="\n"):
         print(char, end='', flush=True)
         sleep(delay)
     print(end)
+
 
 
 def userStart():
@@ -26,8 +28,52 @@ def userStart():
         slowPrint('Do you understand the rules? If not, enter \'Teach me\'.', 0.1)
         tutorial = str(input("Enter:"))
 
-userStart()
+# user decide difficulty
 
+def userDifficulty(cheatword):
+    """Allows a user to choose from Easy, Hard and Impossible"""
+    possDiff = ['easy','hard','impossible',cheatword]
+    slowPrint("What difficulty would you like? Choose from Easy, Hard, and Impossible.")
+    difficulty = input("Difficulty:").lower()
+    while difficulty not in possDiff:
+        slowPrint("Please try again. You may have made a typo.")
+        difficulty = input("Difficulty:").lower()
+    if difficulty in possDiff:
+        slowPrint(f"You have chosen {difficulty}, good luck!")
+        if difficulty == "impossible":
+            slowPrint("You have made a big mistake. Mwahahaha!", .1)
+        elif difficulty == cheatword:
+            slowPrint("Well, since you asked SO nicely, and I promised I would go easy if you asked nicely, I'll go easy on you.\nYou have the max number of guesses, 27.")
+    return difficulty
+
+# user declare guesses
+
+def validGuess(difficulty, guesses, maxG):
+    if difficulty == "hard":
+        while not int(guesses) or guesses > 7:
+            return False
+        return True
+    elif difficulty == cheatword:
+        pass
+    else:
+        while not int(guesses) or guesses > maxG:
+            return False
+        return True
+
+
+def getGuesses(difficulty):
+    if difficulty == "easy" or difficulty == "impossible":
+        slowPrint("How many guesses would you like? The maximum is 26 (or is it?). ")
+        guesses = (input("Guesses:"))
+        while not validGuess(difficulty):
+            slowPrint("That is an invalid amount of guesses. Please enter a number ")
+            guesses = input("Guesses:")
+    if difficulty == "hard":
+        slowPrint("Due to your difficulty choice, you only get 6 or less guesses. Please choose how many guesses you would like")
+        guesses = int(input("Guesses:"))
+        while not validGuess(difficulty):
+            slowPrint("I apologize, but if I gave you more than 6 guesses, this game wouldn't be so hard, now WOULD it?\nYou chose your own difficulty, after all!\nPlease choose a number less than 6.")
+            guesses = int(input("Guesses:"))
 
 # print game status "_ _ _ _"
 
@@ -40,5 +86,10 @@ def gameStatus():
 # generate new families {"_ _ _ _": listOfWords}
 
 
+def main():
+    userStart()
+    difficulty = userDifficulty(cheatword)
+    maxGuesses = getGuesses(difficulty)
 
-
+if __name__ == "__main__":
+    main()
