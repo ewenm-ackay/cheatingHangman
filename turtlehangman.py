2287b2368c3c4f2e4a700d603f1ec1cd2bf55081
Johnny Tan <150210479+chocojohnni@users.noreply.github.com>
2023-12-09 23:18:26 -0500

    Update turtlehangman.py

--- turtlehangman.py
+++ turtlehangman.py
@@ -22,29 +22,34 @@ def DrawLeftArm():
     t.speed(2)
     t.lt(10)
     t.fd(60)
-
+    
+# DrawRightArm draws the right arm of the hanging man.
 def DrawRightArm():
     t.speed(2)
     t.lt(170)
     t.fd(60)
 
+# DrawLeftLeg draws the left leg of the hanging man.
 def DrawLeftLeg():
     t.speed(2)
     t.rt(45)
     t.fd(60)
 
+# DrawRightLeg draws the right leg of the hanging man.
 def DrawRightLeg():
     t.speed(2)
     t.rt(135)
     t.fd(60)
 
+# DrawToes draws each toe of the hanging man.
 def DrawToes():
     t.speed(2)
     for i in range(5):
         t.fd(30)
         t.bk(30)
         t.rt(20)
-
+        
+# DrawFingers draws each finger of the hanging man.
 def DrawFingers():
     t.speed(2)
     for i in range(5):
